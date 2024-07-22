e908519589b926fd6478e53501902b1a0acc8125
rkamn <rakeshkumar458458@gmail.com>
2024-07-23 00:46:42 +0530

    upgating files of currency converter

--- assets/currencyConverter/app.js
+++ assets/currencyConverter/app.js
@@ -9,10 +9,9 @@ const toCurr = document.querySelector('.to select');
 const msg = document.querySelector('.msg');
 const perUnitMsg = document.querySelector('.perUnitMsg');
 
-document.addEventListener('load',(evt)=>{
+window.addEventListener('load',(evt)=>{
     updateExchangeRate(evt);
-})
-
+});
 
 
 for(let select of dropdowns){
@@ -31,38 +30,44 @@ for(let select of dropdowns){
     // onchange in dropdown -> invoke change
     select.addEventListener('change', (evt) =>{
         updateFlag(evt.target);
+        //console.log(evt.target);
     });
 }
 //flag change logic
 const updateFlag = (element) =>{
+     //console.log(element);
      let currCode = element.value;
      let countryCode = countryList[currCode];
      let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
      let img = element.parentElement.querySelector('img');
      img.src = newSrc;
-     console.log('new flag image updated selected');
+     //img.alt = countryCode+"-"
+     console.log('new flag image updated selected =',countryCode);
 }
 
 btnForm.addEventListener('click', (evt)=>{
-    evt.preventDefault();
+    evt.preventDefault(); // stop stomatic behaviour of form
     updateExchangeRate(evt);
+    //console.log(evt.target);
 
 });
 
 const updateExchangeRate = async (evt) =>{
     let amount = document.querySelector('.amount input');
-    // default value for invalid input
+    // default value 1 for invalid input
     let amtVal = amount.value;
     console.log(amtVal);
     if(amtVal ==='' || amtVal <=0){
         amtVal = 1;
         amount.value = '1';
     }
+
     //country-by-currency-code.json';
     console.log(fromCurr.value, toCurr.value);
     let url = `${BASE_URL}${fromCurr.value.toLowerCase()}.json`;
     let response = await fetch(url);
     let data = await response.json();
+    //console.log(data);
     let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
     console.log(rate);
     perUnitMsg.innerText = `1 ${fromCurr.value} = ${rate} ${toCurr.value}`
@@ -70,3 +75,6 @@ const updateExchangeRate = async (evt) =>{
     msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
 
 } 
+
+
+
